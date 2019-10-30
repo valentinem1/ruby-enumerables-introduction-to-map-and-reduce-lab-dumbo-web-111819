8b697bb8695438041ca013a0ec0d379b56918e15
Valentine Maillard <valentinemaillard1@gmail.com>
2019-10-30 16:47:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -16,7 +16,7 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point=0)
- source_array.inject(0){
+ source_array.inject(starting_point){
  |starting_point, x| starting_point + x
  }
  #source_array.reduce(starting_point, :+)
