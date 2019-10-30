7ecf8b8e29e0db7aa8177db18e500da605e55e28
Valentine Maillard <valentinemaillard1@gmail.com>
2019-10-30 16:46:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -19,7 +19,7 @@ def reduce_to_total(source_array, starting_point=0)
  source_array.inject(0){
  |starting_point, x| starting_point + x
  }
- source_array.reduce(starting_point, :+)
+ #source_array.reduce(starting_point, :+)
 end
 
 
