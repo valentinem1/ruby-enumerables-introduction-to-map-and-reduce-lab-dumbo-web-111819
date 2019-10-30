e984ab9657e2a9b6fcded3b9a916023b3a1bd874
Valentine Maillard <valentinemaillard1@gmail.com>
2019-10-30 16:39:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
 # My Code here....
+def map_to_negativize(source_array)
+  source_array.collect{|x| x * -1}
+end
+
+def map_to_no_change(source_array)
+  p source_array
+end  
+
+def map_to_double(source_array)
+  source_array.collect{|x| x * 2}
+end
+
+def map_to_square(source_array)
+  source_array.collect{|x| x * x}
+end
+
+def reduce_to_total(source_array, starting_point=0)
+  source_array.inject{
+  |starting_point, x| starting_point + x
+  }
+  return source_array
+end
+
+
